bf2fcf0a9c9b7d30cb1e7fc3921adacc8e699010
sapientia1007 <179jh@naver.com>
2025-04-22 12:53:26 +0900

    fix: 예약어로 인한 엔티티 이름 변경

--- src/main/java/com/wid/elevenmarket/model/Orders.java
+++ src/main/java/com/wid/elevenmarket/model/Orders.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Order extends BaseTimeEntity {
+public class Orders extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
