571b28d67b5f800b5942c589952894bada5a5504
sapientia1007 <179jh@naver.com>
2025-05-26 16:31:44 +0900

    fix: Lock 타임아웃 설정

--- src/main/java/com/wid/elevenmarket/persistence/ProductRepository.java
+++ src/main/java/com/wid/elevenmarket/persistence/ProductRepository.java
@@ -2,9 +2,11 @@ package com.wid.elevenmarket.persistence;
 
 import com.wid.elevenmarket.model.Product;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
@@ -13,5 +15,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM Product p where p.id = :id")
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")}) // 3초 대기
     Optional<Product> findProductByIdWithPessimisticLock(@Param("id") Long id);
 }
