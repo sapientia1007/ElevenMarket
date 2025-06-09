14c0a9ad2531db02c5a8c31ec3111e6e49af11c6
sapientia1007 <179jh@naver.com>
2025-06-09 16:26:26 +0900

    fix: 스케줄러 파라미터 최소화 및 단일 기능 메서드로 정리

--- src/main/java/com/wid/elevenmarket/scheduler/AuctionScheduler.java
+++ src/main/java/com/wid/elevenmarket/scheduler/AuctionScheduler.java
@@ -26,39 +26,33 @@ public class AuctionScheduler {
 
         LocalDateTime now = LocalDateTime.now();
 
-        int updatedCount = auctionRepository.updateStatusFromPendingToLive(
-                AuctionStatus.LIVE, AuctionStatus.PENDING, now);
+        int updatedCount = auctionRepository.updateStatusFromPendingToLive(now);
         System.out.println("경매 시작 상태 변경 건수: " + updatedCount);
     }
 
     @Scheduled(fixedRate = 10_000)
     @Transactional
-    public void endWithWinnerAuctions() {
+    public void endAuctions() {
 
         LocalDateTime now = LocalDateTime.now();
 
         int updatedCount = auctionRepository.updateStatusFromLiveToEnd(now);
         System.out.println("경매 종료 처리 건수: " + updatedCount);
+
         List<Auction> AuctionsEnd = auctionRepository.findByStatus(AuctionStatus.END);
 
         for (Auction auction : AuctionsEnd) {
-            try {
+            if (bidRepository.findHighestBidByAuctionId(auction).isPresent()) {
                 Bid highestBid = bidRepository.findHighestBidByAuctionId(auction).orElseThrow();
                 auction.setAuctionWinner(highestBid.getBidder());
                 auction.changeStatus(AuctionStatus.WINNER);
-            } catch (Exception e) {
-                System.out.println("경매 ID "+auction.getId()+"  처리 실패: " + e.getMessage());
-                throw e;
+                System.out.println("경매 ID "+auction.getId()+"  처리 성공: ");
+            } else {
+                auction.changeStatus(AuctionStatus.FAILED);
+                System.out.println("경매 ID "+auction.getId()+"  처리 실패: ");
             }
+            auctionRepository.save(auction);
         }
     }
 
-    @Scheduled(fixedRate = 10_000)
-    @Transactional
-    public void cancelWithoutWinnerAuctions() {
-        LocalDateTime now = LocalDateTime.now();
-
-        int updatedCount = auctionRepository.updateStatusFromLiveToEndWithoutWinner(now);
-        System.out.println("낙찰자 없는 경매 실패 처리 건수: " + updatedCount);
-    }
 }
